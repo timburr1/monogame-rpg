b2b67cf946d48c068160d482883918b60fcc92d1
Tim <tim.r.burr@gmail.com>
2022-06-14 12:27:18 -0600

    Tutorial 11: gameplay screen + player

--- README.md
+++ README.md
@@ -6,4 +6,6 @@
 
 Based on the [C# Monogame RPG Made Easy](https://www.youtube.com/playlist?list=PLHJE4y54mpC5hrlDv8yFHPfrSNhqFoA0h) YouTube playlist, by CodingMadeEasy. There are many more MonoGame tutorials [here](https://docs.monogame.net/articles/tutorials.html).
 
+[Retro Sprite Creator](https://retro-sprite-creator.nihey.org/character/eyJiYXNlIjoiMWViOTI5ZjEiLCJib2R5IjoiOTg2NmJhNWEifQ==)
+
 If your XML files are not loading correctly, double-check the corresponding file in monogame-rpg\bin\Debug\netcoreapp3.1\Load
---
+++ monogame-rpg/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace monogame_rpg
+{
+    public class Player
+    {
+        public Image Image;
+        public Vector2 Velocity;
+        public float MoveSpeed;
+
+        public Player()
+        {
+            Velocity = Vector2.Zero;
+        }
+        public void LoadContent()
+        {
+            Image.LoadContent();
+        }
+
+        public void UnloadContent()
+        {
+            Image.UnloadContent();
+        }
+
+        public void Update(GameTime gameTime) 
+        {
+            if (InputManager.Instance.KeyDown(Keys.Down))
+                Velocity.Y = MoveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            else if (InputManager.Instance.KeyDown(Keys.Up))
+                Velocity.Y = -MoveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            else
+                Velocity.Y = 0;
+
+            if (InputManager.Instance.KeyDown(Keys.Right))
+                Velocity.X = MoveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            else if (InputManager.Instance.KeyDown(Keys.Left))
+                Velocity.X = -MoveSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            else
+                Velocity.X = 0;
+
+            Image.Position += Velocity;
+        }
+
+        public void Draw(SpriteBatch spriteBatch) 
+        {
+            Image.Draw(spriteBatch);
+        }
+    }
+}
